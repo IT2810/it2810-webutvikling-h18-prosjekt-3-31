66df43f876ca24e892e67f73042d37cb08852be2
Jack Syvertsen <jmsyvert@stud.ntnu.no>
2018-09-28 15:02:05 +0200

    Added stack to navigator

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -6,6 +6,7 @@ import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import TodoScreen from '../screens/TodoScreen';
 
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
@@ -38,7 +39,23 @@ LinksStack.navigationOptions = {
     />
   ),
 };
+const TodoStack = createStackNavigator({
+  Todo: TodoScreen,
+});
 
+TodoStack.navigationOptions = {
+  tabBarLabel: 'Todo',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
 const SettingsStack = createStackNavigator({
   Settings: SettingsScreen,
 });
@@ -57,4 +74,5 @@ export default createBottomTabNavigator({
   HomeStack,
   LinksStack,
   SettingsStack,
+  TodoStack,
 });
