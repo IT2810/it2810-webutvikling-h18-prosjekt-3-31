3039bba5b1e82d23cf539539e67f80eb7e194395
Jack Syvertsen <jmsyvert@stud.ntnu.no>
2018-10-02 12:04:54 +0200

    #9, #11 Agenda with calendar, items possible to add and render

--- screens/CalendarsScreen.js
+++ screens/CalendarsScreen.js
@@ -11,36 +11,63 @@ import {Agenda} from 'react-native-calendars';
 export default class CalendarsScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {items: {}};
     this.onDayPress = this.onDayPress.bind(this);
   }
 
   render() {
     return (
       <ScrollView style={styles.container}>
-        <Calendar
-          style={styles.calendar}
-          onDayPress={this.onDayPress}
-          current={'2012-05-16'}
-          markingType={'multi-dot'}
-          markedDates={{
-            [this.state.selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}
-          }}
-          
-          hideArrows={false}
-        />
-        <Agenda
-          selected={'2012-05-16'}
-          pastScrollRange={50}
-          futureScrollRange={50}
-          renderItem={(item, firstItemInDay) => {return (<View />);}}
-          renderDay={(day, item) => {return (<View />);}}
-          renderEmptyDate={() => {return (<View />);}}
-          // Set this true while waiting for new data from a refresh
-          refreshing={false}
-          />
+        
+        <Agenda style={styles.Agenda}
+        items={this.state.items}
+        loadItemsForMonth={this.loadItems.bind(this)}
+        selected={'2017-05-16'}
+        renderItem={this.renderItem.bind(this)}
+        renderEmptyDate={this.renderEmptyDate.bind(this)}
+        rowHasChanged={this.rowHasChanged.bind(this)}
+        // markingType={'period'}
+        // markedDates={{
+        //    '2017-05-08': {textColor: '#666'},
+        //    '2017-05-09': {textColor: '#666'},
+        //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
+        //    '2017-05-21': {startingDay: true, color: 'blue'},
+        //    '2017-05-22': {endingDay: true, color: 'gray'},
+        //    '2017-05-24': {startingDay: true, color: 'gray'},
+        //    '2017-05-25': {color: 'gray'},
+        //    '2017-05-26': {endingDay: true, color: 'gray'}}}
+         // monthFormat={'yyyy'}
+         // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+        //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
+      />
       </ScrollView>
     );
+  }
+
+  loadItems(day) {
+    setTimeout(() => {
+      for (let i = -15; i < 15; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = this.timeToString(time);
+        if (!this.state.items[strTime]) {
+          this.state.items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 3);
+          for (let j = 0; j < numItems; j++) {
+            this.state.items[strTime].push({
+              name: 'Item for ' + strTime,
+              height: Math.max(50, Math.floor(Math.random() * 150))
+            });
+          }
+        }
+      }
+      //console.log(this.state.items);
+      const newItems = {};
+      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
+      this.setState({
+        items: newItems
+      });
+    }, 1000);
+    // console.log(`Load Items for ${day.year}-${day.month}`);
   }
 
   onDayPress(day) {
@@ -48,6 +75,28 @@ export default class CalendarsScreen extends Component {
       selected: day.dateString
     });
   }
+
+  renderItem(item) {
+    return (
+      <View style={[styles.item, {height: item.height}]}><Text>{item.name}</Text></View>
+    );
+  }
+
+  renderEmptyDate() {
+    return (
+      <View style={styles.emptyDate}><Text>This is empty date!</Text></View>
+    );
+  }
+
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -66,6 +115,23 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    backgroundColor: 'gray'
+    backgroundColor: 'gray',
+    paddingTop: 23
+  },
+  Agenda:{
+    
+  },
+  item: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
+  },
+  emptyDate: {
+    height: 15,
+    flex:1,
+    paddingTop: 30
   }
 });
